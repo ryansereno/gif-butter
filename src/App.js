1fc9474b4a8ec5c747870875ee15bc1aca8356cf
R <ryansereno@protonmail.com>
2022-10-10 10:40:37 -0400

    added file upload field

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
+import { useDropzone } from "react-dropzone";
+import React, {useState} from 'react'
 import JarLogo from "./components/UI/JarLogo";
 
 function App() {
+  const [file, setFile] = useState();
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: "video/*",
+    onDrop: (acceptedFile) => {
+      setFile(
+        Object.assign(acceptedFile, {
+          preview: URL.createObjectURL(acceptedFile),
+        })
+      );
+    },
+  });
   return (
     <div className="container">
       <header>
         <JarLogo />
-        <h1>gif<br/> butter</h1>
+        <h1>
+          gif
+          <br /> butter
+        </h1>
       </header>
       <main>
-        <h2><i>Fast video to gif conversion</i></h2>
-        <div className="card file-upload">upload file</div>
+        <h2>
+          <i>Fast video to gif conversion</i>
+        </h2>
+        <div className="card file-upload" {...getRootProps()}>
+          upload file
+          <input {...getInputProps()} />
+        </div>
       </main>
       <footer>
         <p>Made by Ryan</p>
